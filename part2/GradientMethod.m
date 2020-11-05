227d2ef24d6e31d6ec9f9b998d08115ac377d5ad
Leonardo Pedroso <leonardodalousa@gmail.com>
2020-11-05 21:15:49 +0000

    finished 2. gradient method

---
+++ part2/GradientMethod.m
@@ -0,0 +1,96 @@
+%% Initialization
+clear;
+clc;
+NDataSets = 4;
+
+%% Setup parameters 
+epsl = 1e-6; % stopping criterion
+alpha_hat = 1; %initialization of alpha_k for the backtracking routine
+gamma = 1e-4; % gamma of backtraking routine
+beta = 0.5; % beta of backtraking routine
+maxIt = [1e4; 1e4; 1e4; 1e5]; % maximum number of iterations
+
+%% GD for each data set
+for i = 1:NDataSets
+    %% Upload data
+    load(sprintf("./data%d.mat",i),'X','Y'); % upload data set
+    K = length(Y);
+    n = size(X,1);
+    
+    %% Set up x0 (note that x = [s;r])
+    x0 = [-ones(n,1); 0];
+
+    %% Setup objetive function and gradient
+    h = [X;-ones(1,K)];
+    F = @(x) (1/K)*...
+        sum(log(1+exp((h'*x)'))-Y.*(h'*x)');
+    gradF = @(x) (1/K)*sum((exp((h'*x)')./...
+        (1+exp((h'*x)'))-Y).*h,2);
+ 
+    %% Run GD
+    fprintf("Running gradient descent for dataset %d (n = %d | K = %d).\n",...
+        i,n,K);
+    tic
+    [xGD,ItGD,normGrad] = gradientDescent(F,gradF,x0,epsl,...
+        alpha_hat,gamma,beta,maxIt(i));
+    elapsedTime = toc;
+    if ~isnan(xGD)
+        fprintf("Gradient descent for dataset %d"+...
+        " converged in %d iterations.\n",i,ItGD);
+        fprintf("Elapsed time is %f seconds.\n",elapsedTime);
+        if i<=2
+            fprintf("s = [%g; %g] | r = %g.\n",xGD(1),xGD(2),xGD(3));
+        end
+    else
+        fprintf("Gradient descent for dataset %d "+... 
+            "exceeded the maximum number of iterations.\n",i); 
+        fprintf("Elapsed time is %f seconds.\n",elapsedTime);
+    end
+    save(sprintf("./DATA/GradientDescent/GDsolDataset%d.mat",i),...
+        'xGD','ItGD','normGrad','elapsedTime');
+    
+    %% Plot result
+    if i<= 2
+        figure('units','normalized','outerposition',[0 0 1 1]);
+        set(gca,'FontSize',35);
+        hold on;
+        ax = gca;
+        ax.XGrid = 'on';
+        ax.YGrid = 'on';
+        axis equal;
+        for k = 1:K
+            if Y(k)
+                scatter(X(1,k),X(2,k),200,'o','b','LineWidth',3); 
+            else
+                scatter(X(1,k),X(2,k),200,'o','r','LineWidth',3); 
+            end
+        end
+        %ylim([-4 8]);
+        %xlim([-4 8]);
+        title(sprintf("Dataset %d",i));
+        ylabel('$x_2$','Interpreter','latex');
+        xlabel('$x_1$','Interpreter','latex');
+        x1 = (min(X(1,:)):(max(X(1,:)-min(X(1,:))))/100:max(X(1,:)));
+        plot(x1,(xGD(3)-xGD(1)*x1)/xGD(2),'--g','LineWidth',4);
+        saveas(gcf,sprintf("./DATA/GradientDescent/GDsolDataset%d.fig",i));
+        close(gcf);
+        hold off; 
+    end
+    
+    figure('units','normalized','outerposition',[0 0 1 1]);
+    plot(0:ItGD-1,normGrad,'LineWidth',3);
+    hold on;
+    set(gca,'FontSize',35);
+    ax = gca;
+    ax.XGrid = 'on';
+    ax.YGrid = 'on';
+    title(sprintf("Gradient method | Dataset %d",i));
+    ylabel('$||\Delta f (s_k,r_k)||$','Interpreter','latex');
+    xlabel('$k$','Interpreter','latex');
+    set(gca, 'YScale', 'log');
+    saveas(gcf,sprintf("./DATA/GradientDescent/GDNormGradDataset%d.fig",i));
+    close(gcf);
+    hold off; 
+    
+    
+end
---
+++ part2/gradientDescent.m
@@ -0,0 +1,52 @@
+function [xk,k,normGk] = gradientDescent(F,gradF,x0,epsl,alpha_hat,gamma,beta,maxIt)
+    %% Description
+    % Inputs: 1. F: objective function (as a function handle)
+    %         2. gradF: gradient of teh objective function (as a function 
+    %         handle)
+    %         3. D: data set information
+    %         4. x0: initialization
+    %         5. epsl: stopping criterion
+    %         6. alpha_hat: initialization of alpha_k for the backtracking
+    %         routine
+    %         7. gamma: gamma of backtraking routine
+    %         8. beta: beta of backtraking routine
+    %         9. maxIt: maximum number of iterations
+    % Outputs: 1. x: output of the gradient descent method (returns NaN if
+    %          stopping criterion not met after the maximum number of 
+    %          iterations chosen
+    %          2. k: number of iterations required for convergence if a
+    %          solution was found
+    %          3. normGk: norm of the gradient of the objective function
+    %% Gradient descent routine 
+    k = 0;
+    xk = x0;
+    normGk = zeros(maxIt,1);
+    while k < maxIt
+        gk = gradF(xk); % Compute gradient at xk
+        normGk(k+1) = norm(gk);
+        if normGk(k+1) < epsl % Stopping criterion
+           break; 
+        end
+        % ---------  backtracking routine  ---------
+        alpha_k = alpha_hat;
+        % It is guaranteed that there is convergence, no maximum number of 
+        % iterations needed (obviously for beta < 0)
+        while true 
+            % check if F(alpha_k) < phi(0)+gamma*phi_dot(0)+alpha_k 
+            if F(xk-alpha_k*gk) < F(xk)-gamma*alpha_k*(gk'*gk) 
+                break; % alpha_k found
+            else
+                alpha_k = beta*alpha_k; % Update alpha_k
+            end
+        end
+        xk = xk - alpha_k*gk; % update xk
+        % -------  End backtracking routine  -------
+        k = k + 1; % Increment iteration count
+    end   
+    if k == maxIt
+        % No solution found within the maximum number of iterations
+        xk = NaN;
+    else
+        normGk = normGk(1:k);
+    end
+end
